5e70ee13d5caefbb4dd78971c9b23777db88f68e
veryacademy <35196453+veryacademy@users.noreply.github.com>
2020-09-14 13:37:23 +0100

    nc

---
+++ blog_api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from blog.models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'author', 'excerpt', 'content', 'status')
---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
+    path('api/', include('blog_api.urls', namespace='blog_api')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
